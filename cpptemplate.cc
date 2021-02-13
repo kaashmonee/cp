f5d0cdfbfe3caff6bfe9fb803520051222dff564
Skanda Kaashyap <skandakk@gmail.com>
2021-02-13 11:08:21 -0600

    d2c701pa

--- cpptemplate.cc
+++ cpptemplate.cc
@@ -16,6 +16,21 @@ const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
+
+// log n algorithm to compute the log of some number a with base b
+// this function exists to avoid rounding errors with floats
+ll compute_log(ll a, ll b) {
+    ll ctr = 0;
+    if (b == 1) {
+        return intmax;
+    }
+    while (a > 0) {
+        a /= b;
+        ctr++;
+    }
+    return ctr;
+}
+
 void solve() {
     // write solution here
 }
--- d2c701/a.cc
+++ d2c701/a.cc
@@ -16,8 +16,40 @@ const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
+ll compute_log(ll a, ll b) {
+    ll ctr = 0;
+    if (b == 1) {
+        return intmax;
+    }
+    while (a > 0) {
+        a /= b;
+        ctr++;
+    }
+    return ctr;
+}
+
 void solve() {
-    // write solution here
+    ll a, b;
+    cin >> a >> b;
+
+    ll min = intmax;
+    ll moves = 0;
+
+    for (ll sb = 0; sb <= 30; sb++) {
+        moves = sb;
+        ll tb = b + sb;
+
+        // going to compute the log by hand to avoid floating point mixups
+        ll log_res = compute_log(a, tb);
+
+        moves += log_res;
+        if (moves < min && moves >= 0) {
+            min = moves;
+        }
+    }
+
+    cout<<min<<"\n";
+
 }
 
 
